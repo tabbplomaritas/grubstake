82546743a3bb26d90ac6ad9a475eefde63798773
Tabbatha <tabbatha@DOBETTERs-iMac.local>
2018-11-01 13:50:12 -0400

    created branch, color change test

--- js/base.js
+++ js/base.js
@@ -3,14 +3,28 @@
 $(document).ready(() => {
 
 $(window).bind("scroll", function() { 
-    var $sec1 = $('.intro').offset().top;
-    var $sec2 = $('.about').offset().top;
+    var $intro = $('.intro').offset().top;
+    var $about = $('.about').offset().top;
+    var $workshops = $('.workshops').offset().top;
 
-   if ($(this).scrollTop() > $sec2){ 
+   if ($(this).scrollTop() < $about){ 
        console.log("its happening");
        
+     $(".linkB").css("color", "#fece8a");
+     $("#logo").css("color", "#fece8a");
+   }
+     if ($(this).scrollTop() > $about){ 
+       console.log("its happening");
+       
+     $(".linkB").css("color", "#a5a4a4");
      $("#logo").css("color", "#a5a4a4");
-   }     
+   }
+//    }  else if ($(this).scrollTop() > $intro && $(this).scrollTop() < $about){ 
+//        console.log("its happening");
+       
+//      $(".linkB").css("color", "#a5a4a4");
+//      $(".linkB").css("color", "#a5a4a4");
+//    }     
 //    if ($(this).scrollTop() > $sec2 & $(this).scrollTop() < $sec3){ 
 //      $(".navbar1").fadeIn();   
 //      $(".navbar2").fadeOut();      
