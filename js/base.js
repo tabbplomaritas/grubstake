c39ceb183a645f0e7fec3d787cffa0854d70cebb
Tabbatha <tabbatha@DOBETTERs-iMac.local>
2018-11-02 12:31:36 -0400

    set up workshops section

--- js/base.js
+++ js/base.js
@@ -2,22 +2,85 @@
 
 $(document).ready(() => {
 
+const events = [
+    {
+        date: 'NOV. 30, 2019',
+        venue: 'Traverse City Chamber of Commerce',
+        location: 'Traverse City, MI', 
+        url: '#'
+    },
+     {
+        date: 'NOV. 30, 2019',
+        venue: 'Traverse City Chamber of Commerce',
+        location: 'Traverse City, MI', 
+        url: '#'
+    },
+     {
+        date: 'NOV. 30, 2019',
+        venue: 'Traverse City Chamber of Commerce',
+        location: 'Traverse City, MI', 
+        url: '#'
+    },
+     {
+        date: 'NOV. 30, 2019',
+        venue: 'Traverse City Chamber of Commerce',
+        location: 'Traverse City, MI', 
+        url: '#'
+    }
+]
+
+
+//generate events table
+$(function() {
+    for(let i=0; i < events.length; i++) {
+        let eventRow = `
+        <div class="eventRow"> 
+            <p>${events[i].date}</p>
+            <p>${events[i].venue}</p>
+            <p>${events[i].location}</p>
+            <a href="#">Info</a>
+            <a href=${events[i].url}>Register now</a>
+
+            
+            </div`;
+
+
+         $(".workshopEvents").append(eventRow);
+    }
+});
+
+
 $(window).bind("scroll", function() { 
-    var $sec1 = $('.intro').offset().top;
-    var $sec2 = $('.about').offset().top;
+    const $intro = $('.intro').offset().top -40;
+    const $about = $('.about').offset().top -40;
+    const $workshops = $('.workshops').offset().top - 40;
+    const $logo = $('#logo');
+    const $linkA = $('nav > .linkA');
+    const $linkB = $('nav > .linkB');
+    let $colorPrimary;
+    let $colorSecondary;
+    let $colorTertiary;
+    
 
-   if ($(this).scrollTop() > $sec2){ 
-       console.log("its happening");
+   if ($(this).scrollTop() < $about){ 
+        $logo.css("color", "#fece8a");
+        $linkA.css({"color": "white", "border-bottom": "2px solid white"});
+        $linkB.css({"color": "#fece8a", "border-bottom": "2px solid #fece8a"});
        
-     $("#logo").css("color", "#a5a4a4");
-   }     
-//    if ($(this).scrollTop() > $sec2 & $(this).scrollTop() < $sec3){ 
-//      $(".navbar1").fadeIn();   
-//      $(".navbar2").fadeOut();      
-//    } 
-        
- });
+   } else if ($(this).scrollTop() > $about && $(this).scrollTop() < $workshops){ 
+        $logo.css("color", "#a5a4a4")
+        $linkA.css({"color": "#4cbc8c", "border-bottom": "2px solid #4cbc8c"});
+        $linkB.css({"color": "#a5a4a4", "border-bottom": "2px solid #a5a4a4"});
+        // $('.about').css("padding-top", "15rem");
 
+   } else if ($(this).scrollTop() > $workshops){
+       console.log('WORKSHOP');
+        $logo.css("color", "#21704f")
+        $linkA.css({"color": "white", "border-bottom": "2px solid white"});
+        $linkB.css({"color": "#21704f", "border-bottom": "2px solid #21704f"});
+
+   }
+})
 
 
 })
