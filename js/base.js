9b2d6b06291d7088ab37f07a2b3e6d0262e351d1
Tabbatha <tabbatha@DOBETTERs-iMac.local>
2018-11-27 09:16:52 -0500

    updated niles event date

--- js/base.js
+++ js/base.js
@@ -16,7 +16,7 @@ $(document).ready(() => {
             url: 'https://www.eventbrite.com/e/owosso-mi-local-investing-workshop-tickets-52871391756',
         },
         {
-            date: 'Jan. 24, 2019',
+            date: 'Mar. 14, 2019',
             venue: 'Location TBD',
             location: 'Niles, MI', 
             url: 'https://www.eventbrite.com/e/niles-mi-local-investing-workshop-tickets-52872080817',
